95df8b345d1ed5ac070d975b4542f201ecf6b0f7
Lucas Lim <jityang91@gmail.com>
2024-04-08 21:25:56 +0800

    Fix airdrop amount

--- src/Governance/Services/step0_AirdropGuildToken.ts
+++ src/Governance/Services/step0_AirdropGuildToken.ts
@@ -2,40 +2,59 @@ import { LAMPORTS_PER_SOL, Keypair, PublicKey, TransactionInstruction, Transacti
 import { getAccount, getOrCreateAssociatedTokenAccount } from '@solana/spl-token';
 import { connection } from '../Tools/env';
 import * as bs58 from 'bs58';
-import { getOrCreateAssociatedAccount, getTokenAuthoritySecret, sendTokensTo } from '../../../utils';
+import { getOrCreateAssociatedAccount, getTokenAuthoritySecret, sendTokensTo, sleep } from '../../../utils';
+import { attempt } from 'lodash';
 
-export const airdropGuildToken = async(mintPk: PublicKey, recipientPk: PublicKey) => {
+export const airdropGuildToken = async(mintPk: PublicKey, ownerPk: PublicKey) => {
+    let attempts = 3;
+    let status = 0;
+    do {
+        attempts--;
 
-    //  Create a keypair from the secret key
-    const mintAuthority = Keypair.fromSecretKey(bs58.decode(getTokenAuthoritySecret()));
+        //  Create a keypair from the secret key
+        const mintAuthority = Keypair.fromSecretKey(bs58.decode(getTokenAuthoritySecret()));
 
-    // Create a new account to hold the tokens
-    const associatedTokenTo = await getOrCreateAssociatedTokenAccount(connection, mintAuthority, mintPk, recipientPk);
-
-    // set airdrop amount
-    const airdropAmount = 1000;
-
-    // Get account info
-    try {
-        const balanceBefore = await getAccount(connection, associatedTokenTo.address);
-        console.log(`balanceBefore: ${balanceBefore.amount.toString()}`);
-        // if already got balance
-        if (Number(balanceBefore.amount.toString()) > 0) {
-            return 0;
+        let associatedTokenTo = null;
+        try {
+            // Create a new account to hold the tokens
+            associatedTokenTo = await getOrCreateAssociatedTokenAccount(connection, mintAuthority, mintPk, ownerPk);
+        } catch(e) {
+            status = 0;
+            console.log(e);
+            continue;
         }
-    } catch(e) {
-        console.log(e);
-        return 0;
-    }
 
-    try {
-        const tx = await sendTokensTo(recipientPk.toBase58(), mintPk.toBase58(), 1, airdropAmount, mintAuthority);
+        // set airdrop amount
+        const airdropAmount = 1000 * (10 ** 9);
 
-        console.log(`Airdropped ${airdropAmount} tokens to ${recipientPk.toBase58()}`);
-        console.log(`https://solscan.io/tx/${tx}?cluster=devnet`);
-        return 1;
-    } catch(e) {
-        console.log(e);
-        return 0;
-    }
+        // Get account info
+        try {
+            const balanceBefore = await getAccount(connection, associatedTokenTo.address);
+            console.log(`balanceBefore: ${balanceBefore.amount.toString()}`);
+            // if already got balance
+            if (Number(balanceBefore.amount.toString()) > 0) {
+                status = 0;
+                break;
+            }
+        } catch(e) {
+            status = 0;
+            console.log(e);
+            continue;
+        }
+
+        try {
+            const tx = await sendTokensTo(ownerPk.toBase58(), mintPk.toBase58(), 1, airdropAmount, mintAuthority);
+
+            console.log(`Airdropped ${airdropAmount} tokens to ${ownerPk.toBase58()}`);
+            console.log(`https://solscan.io/tx/${tx}?cluster=devnet`);
+            status = 1;
+            break;
+        } catch(e) {
+            status = 0;
+            console.log(e);
+            continue;
+        }
+    } while(attempts > 0 && status == 0);
+
+    return status;
 }
